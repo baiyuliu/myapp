a5350f3e115b64cfed44a967a698f5b5c2de9eae
ericliu-ff <51595058+baiyuliu@users.noreply.github.com>
2023-06-18 11:37:24 +0800

    Update test_history_controller.py

--- server/swagger_server/test/test_history_controller.py
+++ server/swagger_server/test/test_history_controller.py
@@ -25,6 +25,7 @@ class TestHistoryController(BaseTestCase):
                        'Response body is : ' + response.data.decode('utf-8'))
 
 
+
 if __name__ == '__main__':
     import unittest
     unittest.main()
