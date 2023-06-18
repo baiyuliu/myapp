e6b30ac066d55e4e2e19094c824cef8d7b5b7d28
ericliu-ff <51595058+baiyuliu@users.noreply.github.com>
2023-06-18 18:14:54 +0800

    Create test_health.py

---
+++ server/swagger_server/test/test_health.py
@@ -0,0 +1,15 @@
+import unittest
+from unittest.mock import MagicMock
+from controllers.health_controller import health_check
+
+class TestHealthCheck(unittest.TestCase):
+    def test_health_check(self):
+        # 调用 health_check 函数
+        response = health_check()
+
+        # 断言返回的结果是否正确
+        self.assertEqual(response, 'application health!')
+
+if __name__ == '__main__':
+    unittest.main()
+
